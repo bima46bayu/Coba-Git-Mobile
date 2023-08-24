e5917f709d01ea05fb9b7baf288859240ddc20a9
BIMA <bima46bayu25@gmail..com>
2023-08-24 10:28:02 +0700

    auuuuuu

--- app/src/main/java/com/example/pertemuan1/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan1/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.pertemuan1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.example.pertemuan1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var number = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
     }
-//    Bima Bayu Sofana
+
 }
